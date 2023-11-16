52031dd2db5aa718669b844167eb85a0fcf2d875
Pratyush Singh <singhpratyush121@gmail.com>
2023-11-16 22:14:34 +0530

    Created first widget class

--- first/lib/main.dart
+++ first/lib/main.dart
@@ -4,30 +4,38 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 29, 5, 97),
-                Color.fromARGB(255, 60, 11, 196),
-                Color.fromARGB(255, 95, 39, 249)
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                  color: Colors.black45,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 25.0),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 29, 5, 97),
+            Color.fromARGB(255, 60, 11, 196),
+            Color.fromARGB(255, 95, 39, 249)
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Color.fromARGB(251, 244, 243, 243),
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
